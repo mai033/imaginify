7cebc372348b34f6d58054c393ae94d221e95697
mai033 <145622463+mai033@users.noreply.github.com>
2024-05-25 14:55:59 -0700

    update user type

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -62,12 +62,21 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
+    // const user = {
+    //   clerkId: id,
+    //   email: email_addresses[0].email_address,
+    //   username: username!,
+    //   firstName: first_name,
+    //   lastName: last_name,
+    //   photo: image_url,
+    // };
+
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      username: username ?? '', // Handle null or undefined username
+      firstName: first_name ?? '', // Handle null or undefined first name
+      lastName: last_name ?? '', // Handle null or undefined last name
       photo: image_url,
     };
 
@@ -89,10 +98,17 @@ export async function POST(req: Request) {
   if (eventType === 'user.updated') {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
+    // const user = {
+    //   firstName: first_name,
+    //   lastName: last_name,
+    //   username: username!,
+    //   photo: image_url,
+    // };
+
     const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
+      firstName: first_name ?? '', // Handle null or undefined first name
+      lastName: last_name ?? '', // Handle null or undefined last name
+      username: username ?? '', // Handle null or undefined username
       photo: image_url,
     };
 
